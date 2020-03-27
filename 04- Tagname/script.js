ae1fa7c277bb45f65de37952d87e65544bb6118b
Owlynn <contact@jessalynn.fr>
2020-03-27 15:01:01 +0100

    commit tagname

---
+++ 04- Tagname/script.js
@@ -0,0 +1,10 @@
+let titreh1 = document.getElementsByTagName('h1');
+let para = document.getElementsByTagName('p');
+
+for (valeur of titreh1) {
+    valeur.style.color = "red";
+}
+
+for (valeur of para) {
+    valeur.style.color = "blue";
+}
